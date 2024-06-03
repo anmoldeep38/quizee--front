77027a2c1c539b88da49a3cf7445f5917a4cb2fa
Anmol Deep <anmoldeep38@gmail.com>
2024-06-03 17:23:56 +0530

    vercel.json

--- src/helpers/AxiosInstance.js
+++ src/helpers/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000'; 
-
+//const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000'; 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const axiosInstance = axios.create()
 
 axiosInstance.defaults.baseURL = BASE_URL
