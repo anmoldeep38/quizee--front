176016319527306afe77b990f7d5fd282bc77f7a
Anmol Deep <87927563+anmoldeep38@users.noreply.github.com>
2024-06-03 22:52:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-  //   proxy: {
-  //      '/api': {
+  proxy: {
+      '/api': {
   //   //     target: 'http://localhost:5000',
   //   //   // target: 'https://swiptory-i2xk.onrender.com',
-  //        target:'https://quizee-backen-3.onrender.com/',
-  //       secure: false,
-  //      },
-  //    },
+         target:'https://quizee-backen-3.onrender.com/',
+         secure: false,
+        },
+      },
   },
   plugins: [react()],
 })
